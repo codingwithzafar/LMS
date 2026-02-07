f174e3b6f8c7355774e7451d9b2b86133f11ec83
Ulugbek <ulugbek@example.com>
2026-02-07 09:37:40 +0300

    Fix create_admin: require username

--- backend/users/management/commands/create_admin.py
+++ backend/users/management/commands/create_admin.py
@@ -9,21 +9,28 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         User = get_user_model()
 
+        username = os.getenv("ADMIN_USERNAME")
         email = os.getenv("ADMIN_EMAIL")
         password = os.getenv("ADMIN_PASSWORD")
 
-        if not email or not password:
+        if not username or not email or not password:
             self.stdout.write(self.style.WARNING(
-                "ADMIN_EMAIL or ADMIN_PASSWORD not set. Skipping create_admin."
+                "ADMIN_USERNAME / ADMIN_EMAIL / ADMIN_PASSWORD not set. Skipping create_admin."
             ))
             return
 
-        user = User.objects.filter(email=email).first()
+        user = User.objects.filter(username=username).first() or User.objects.filter(email=email).first()
 
         if user is None:
-            user = User.objects.create_superuser(email=email, password=password)
+            user = User.objects.create_superuser(
+                username=username,
+                email=email,
+                password=password
+            )
             self.stdout.write(self.style.SUCCESS("Created admin user."))
         else:
+            user.username = username
+            user.email = email
             user.set_password(password)
             user.is_staff = True
             user.is_superuser = True
